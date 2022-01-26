5c9d96c7f5751bda987ae48eb9c354d4445f3e2d
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2022-01-26 19:39:23 +0100

    First build

--- main.lua
+++ main.lua
@@ -1,3 +1,20 @@
+WINDOW_WIDTH = 1260
+WINDOW_HEIGHT = 720
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
 function love.draw()
-    love.graphics.print('Hello world', 400, 300)
+    love.graphics.printf(
+        'Hello pong!',
+        0,
+        WINDOW_HEIGHT / 2 - 6,
+        WINDOW_WIDTH,
+        'center'
+    )
 end
